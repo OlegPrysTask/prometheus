9c9aead4be16e94e23ea3bdcc05dfdfe1ad1fcdf
OlegPrysTask <58907539+OlegPrysTask@users.noreply.github.com>
2024-02-22 15:56:46 +0200

    Update check_upswot.bash

--- check_upswot.bash
+++ check_upswot.bash
@@ -1,6 +1,1 @@
-#/bin/bash
-check secret tool for upswot = true 
-SHA_SUN_SECRET = 374627327467236dhfgdhjfgdjh372647236dfjshg
-SECOND_SECRET = sa-jdhfsd749dshfds45487fjhd.json
-asasas a
-rerer
+empty file
