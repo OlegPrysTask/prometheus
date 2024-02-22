ed0dc891ae7de8ce139ece75a531c6a6235faa28
OlegPrysTask <58907539+OlegPrysTask@users.noreply.github.com>
2024-02-22 12:43:23 +0200

    Update check_upswot.bash

--- check_upswot.bash
+++ check_upswot.bash
@@ -3,3 +3,4 @@ check secret tool for upswot = true
 SHA_SUN_SECRET = 374627327467236dhfgdhjfgdjh372647236dfjshg
 SECOND_SECRET = sa-jdhfsd749dshfds45487fjhd.json
 3rd time attend = very_sensitive_upswot_password
+4 time - domain name 
